65ddb36b3b240fddfee2ae210dbdaf45fef2c5da
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-06-28 14:06:24 +0200

    ajax requests with fetch

--- js/promises.js
+++ js/promises.js
@@ -8,6 +8,7 @@
 // resolve and reject methods are provided for us to use
 // whenever we get a rejected promise, we need to handle it or we get an error.
 
+/*
 
 // Create a promise
 const promise = new Promise((resolve, reject) => {
@@ -32,8 +33,36 @@ promise
     })
 
 console.log(promise);
+*/
 
+// THE LECTURER IS NOT THE BIGGEST FAN OF FETCH
+// HE RECOMMENDS USING XIOS OR...
+// LOOK FOR THAT!!
 
+const url = "https://jsonplaceholder.typicode.com/posts/";
+
+fetch(url)
+    // one of the big complaints of fetch is that the data argument here 
+    // is not actually the result object (object of data)
+    // instead it is the response that we get back from the server
+   
+    /*
+    .then((data) => {
+        console.log(data);
+    })*/  
+
+    // So to actually get hands on the data, you have to call a method on it
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(() => {
+        console.log("here wee go");
+    })
+    .then(() => {
+        console.log("come on babby!");
+    })
+    .catch(() => {
+        console.log("uh oh, something went wrong!!");
+    })
 
 
 
